ff0d09b4565ca521149e70bfcac794437f86d6fb
Cristian Cortés <cristiansk311@gmail.com>
2018-12-19 23:34:53 -0500

    Send patient, formularion and ingredients info to the service that stores it on database

--- src/components/IngredientItemComponent.js
+++ src/components/IngredientItemComponent.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import { ListGroupItem, Row, Col } from 'react-bootstrap';
+import { ListGroupItem, Row, Col, Button, Alert } from 'react-bootstrap';
 
 export default class IngredientItemComponent extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            percentage: this.props.item.percentage
+            percentage: this.props.item.percentage,
+            showDescription: false,
+            descriptionButtonLabel: 'Show'
         }
+    }
+
+    componentDidMount(){
+        console.log(this.props.item.minimum_percentage, this.props.item.maximum_percentage);
     }
 
     changeValue(e){
         this.setState({ percentage: e.target.value });
+        this.props.updateIngredientValue(this.props.item.ingredient_id, e.target.value);
     }
 
     renderRangeInput(){
@@ -23,8 +30,14 @@ export default class IngredientItemComponent extends Component {
             <ListGroupItem>
                 <Row className='ingredient-item'>
                     <Col xs={12} sm={4} md={4} lg={4}>{ this.props.item.name }</Col>
-                    <Col xs={12} sm={4} md={4} lg={4}>{ this.props.item.name }</Col>
                     <Col xs={12} sm={4} md={4} lg={4}>{ this.renderRangeInput() }</Col>
+                    <Col xs={12} sm={4} md={4} lg={4}><Button bsStyle='primary' onClick={ () =>  this.setState({ showDescription: this.state.showDescription ? false : true, descriptionButtonLabel: this.state.showDescription ? 'Show' : 'Hide' }) }>{ this.state.descriptionButtonLabel } ingredient description</Button></Col>
+                </Row>
+                <Row className='ingredient-item'>                    
+                    <Col xs={12} sm={12} md={12} lg={12}><hr/></Col>
+                </Row>
+                <Row className='ingredient-item'>                    
+                    <Col xs={12} sm={12} md={12} lg={12}>{ this.state.showDescription ? <Alert bsStyle='warning'>{ this.props.item.description }</Alert> : null }</Col>
                 </Row>
             </ListGroupItem>
         )
--- src/components/PatientFormComponent.js
+++ src/components/PatientFormComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ListGroup, Row, Col, Button, Alert, FormGroup, FormControl, ControlLabel, PanelGroup } from 'react-bootstrap' ;
+import { ListGroup, Row, Col, Button, Alert, FormGroup, FormControl, ControlLabel, Modal } from 'react-bootstrap' ;
 import axios from 'axios';
 
 import IngredientItemComponent from './IngredientItemComponent';
@@ -10,17 +10,22 @@ export default class PatientFormController extends Component {
 
     constructor(){
         super();
-        
         this.state = {
-          formulations: [],
-          validationErrorMessage: '',
-          showErrorValidationMessage: false,
-          formulationIngredients: []
+            show: false,
+            alertType: 'danger',
+            formulations: [],
+            validationErrorMessage: '',
+            showErrorValidationMessage: false,
+            formulationIngredients: []
         }
     }
     
     componentDidMount(){
         this.loadFormulations();
+    }
+
+    handleClose(){
+        //this.setState({ show: false });
     }
     
     loadFormulations(){
@@ -31,7 +36,7 @@ export default class PatientFormController extends Component {
             this.setState({ formulations: response.data.data }, () => this.loadIngredients() );
           }
         }).catch((error) => {
-          this.setState({ showErrorValidationMessage: true, validationErrorMessage: 'An error occurs trying to retreive data' })
+          this.setState({ showErrorValidationMessage: true, alertType: 'danger', validationErrorMessage: 'An error occurs trying to retreive data' })
         });
     }
 
@@ -40,11 +45,10 @@ export default class PatientFormController extends Component {
         axios.get(url)
         .then(response => {
           if(response.data.status == 'SUCCESS'){
-              console.log('INGREDIENTS => ', response.data.data)
             this.setState({ ingredients: response.data.data });
           }
         }).catch((error) => {
-          this.setState({ showErrorValidationMessage: true, validationErrorMessage: 'An error occurs trying to retreive data' })
+          this.setState({ showErrorValidationMessage: true, alertType: 'danger', validationErrorMessage: 'An error occurs trying to retreive data' })
         });
     }
     
@@ -53,110 +57,152 @@ export default class PatientFormController extends Component {
     }
 
     loadIngredientsByFormulation(e){
+        let formulationId = e.target.value;
         const url = base_url + 'formulation_ingredients?id=' + e.target.value;
         axios.get(url)
         .then(response => {
-          if(response.data.status == 'SUCCESS'){
-                console.log('formulation_ingredients', response.data.data);
-                this.setState({ formulationIngredients: response.data.data });
-          }
+            console.log('response => ', response.data.status);
+            if(response.data.status == 'SUCCESS'){
+                this.setState({ formulationIngredients: response.data.data, formulationId });
+            }
         }).catch((error) => {
-          this.setState({ showErrorValidationMessage: true, validationErrorMessage: 'An error occurs trying to retreive data' })
+          this.setState({ showErrorValidationMessage: true, alertType: 'danger', validationErrorMessage: 'An error occurs trying to retreive formulations ingredients' })
         });
+    }
+
+    updateIngredientValue(ingredient_id, val){
+        const formulationIngredients = this.state.formulationIngredients;
+        let ingredientInfo = formulationIngredients.find(k => k.ingredient_id == ingredient_id);
+        ingredientInfo.percentage = val;
+        this.setState({ formulationIngredients });
     }
 
     renderIngredientItem(item, index){
         let ingredientInfo = this.state.ingredients.find(k => k.id == item.ingredient_id);
         item.name = ingredientInfo.name;
+        item.description = ingredientInfo.description;
         item.minimum_percentage = ingredientInfo.minimum_percentage;
         item.maximum_percentage = ingredientInfo.maximum_percentage;
-        return <IngredientItemComponent key={index} item={item} index={index} />;
+        return <IngredientItemComponent key={index} item={item} index={index} updateIngredientValue={ (ingredient_id, val) => this.updateIngredientValue(ingredient_id, val) }/>;
     }
       
+    handleSendForm(){
+        let _ids = [];
+        let _percentages = [];
+        this.state.formulationIngredients.forEach(function(item){
+            _ids.push(item.ingredient_id);
+            _percentages.push(item.percentage);
+        });
+
+        let url = base_url + 'reports?name='+this.state.name+'&address='+this.state.address+'&user_date='+this.state.dateOfBirth+'&formulationId='+this.state.formulationId+'&ids[]=' + _ids.join('&ids[]=')+'&percentages[]=' + _percentages.join('&percentages[]=');
+
+        axios.get(url, this.state.formulationIngredients)
+        .then(response => {
+          if(response.data.status == 'SUCCESS'){
+            this.setState({ showErrorValidationMessage: true, alertType: 'success', validationErrorMessage: 'The data has been saved, look at the report: http://localhost:3000/api/v1reports?' + response.data.data })
+          }
+        }).catch((error) => {
+          this.setState({ showErrorValidationMessage: true, alertType: 'danger', validationErrorMessage: 'An error occurs trying to retreive data' })
+        });
+    }
+
     render(){
         return (
             <div>
-            <form>
-                <div><h4>Patient Information</h4><hr/></div>
-                <Row>
-                    <Col xs={12} sm={12} md={12} lg={12}>
-                    { this.state.showErrorValidationMessage ? <Alert bsStyle="danger">{ this.state.validationErrorMessage }</Alert> : null }
-                    </Col>
-                    <Col xs={12} sm={4} md={4} lg={4}>
-                        <FormGroup
-                        controlId="formName"
-                        >
-                            <ControlLabel>Name:</ControlLabel>
-                            <FormControl
-                                type="text"
-                                value={this.state.name}
-                                placeholder="Name"
-                                onChange={(e) => this.setState({name:e.target.value})}
-                            />
-                        </FormGroup>
-                    </Col>
-                    <Col xs={12} sm={4} md={4} lg={4}>
-                        <FormGroup
-                        controlId="formLastname"
-                        >
-                            <ControlLabel>Address:</ControlLabel>
-                            <FormControl
-                                type="text"
-                                value={this.state.address}
-                                placeholder="Address"
-                                onChange={(e) => this.setState({address:e.target.value})}
-                            />
-                        </FormGroup>
-                    </Col>
-                    <Col xs={12} sm={4} md={4} lg={4}>
-                        <FormGroup
-                        controlId="formBirthDay"
-                        >
-                            <ControlLabel>Date of birth:</ControlLabel>
-                            <FormControl
-                                type="text"
-                                value={this.state.dateOfBirth}
-                                placeholder="Date of birth"
-                                onChange={(e) => this.setState({dateOfBirth:e.target.value})}
-                            />
-                        </FormGroup>
-                    </Col>
-                </Row>
+                <form>
+                    <div><h4>Patient Information</h4><hr/></div>
+                    <Row>
+                        <Col xs={12} sm={12} md={12} lg={12}>
+                        { this.state.showErrorValidationMessage ? <Alert bsStyle={ this.state.alertType }>{ this.state.validationErrorMessage }</Alert> : null }
+                        </Col>
+                        <Col xs={12} sm={4} md={4} lg={4}>
+                            <FormGroup
+                            controlId="formName"
+                            >
+                                <ControlLabel>Name:</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    value={this.state.name}
+                                    placeholder="Name"
+                                    onChange={(e) => this.setState({name:e.target.value})}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Col xs={12} sm={4} md={4} lg={4}>
+                            <FormGroup
+                            controlId="formLastname"
+                            >
+                                <ControlLabel>Address:</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    value={this.state.address}
+                                    placeholder="Address"
+                                    onChange={(e) => this.setState({address:e.target.value})}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Col xs={12} sm={4} md={4} lg={4}>
+                            <FormGroup
+                            controlId="formBirthDay"
+                            >
+                                <ControlLabel>Date of birth:</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    value={this.state.dateOfBirth}
+                                    placeholder="Date of birth"
+                                    onChange={(e) => this.setState({dateOfBirth:e.target.value})}
+                                />
+                            </FormGroup>
+                        </Col>
+                    </Row>
 
-                <Row>
-                    <Col xs={12} sm={4} md={4} lg={4}>
-                    </Col>
-                    <Col xs={12} sm={4} md={4} lg={4}>
-                        <FormGroup controlId="formFormulations">
-                            <ControlLabel>Formulations</ControlLabel>
-                            <FormControl componentClass="select" placeholder="select" onChange={ (e) => this.loadIngredientsByFormulation(e) }>
-                                <option value="0" label="Select an option"></option>
-                                { this.state.formulations.map((item) => { return this.renderFormulationItem(item) }) }
-                            </FormControl>
-                        </FormGroup>
-                    </Col>
-                    <Col xs={12} sm={4} md={4} lg={4}>
-                    </Col>
-                </Row>
-                
-                <Row>
-                    <Col xs={12} sm={6} md={6} lg={6}>
+                    <Row>
+                        <Col xs={12} sm={4} md={4} lg={4}>
+                        </Col>
+                        <Col xs={12} sm={4} md={4} lg={4}>
+                            <FormGroup controlId="formFormulations">
+                                <ControlLabel>Formulations</ControlLabel>
+                                <FormControl componentClass="select" placeholder="select" onChange={ (e) => this.loadIngredientsByFormulation(e) }>
+                                    <option value="0" label="Select an option"></option>
+                                    { this.state.formulations.map((item) => { return this.renderFormulationItem(item) }) }
+                                </FormControl>
+                            </FormGroup>
+                        </Col>
+                        <Col xs={12} sm={4} md={4} lg={4}>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xs={12} sm={12} md={12} lg={12}>
+                            <ListGroup>
+                                { this.state.formulationIngredients.map((item, index) => { return this.renderIngredientItem(item, index) }) }   
+                            </ListGroup>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xs={12} sm={12} md={12} lg={12}>
+                            <hr/>
+                        </Col>
+                    </Row>
                     
-                        
-                    </Col>
-                    <Col xs={12} sm={6} md={6} lg={6}>
-                        <FormGroup
-                        controlId="formSend"
-                        >
-                            <Button bsStyle="primary" onClick={ () => this.handleSend() }>Enviar</Button>
-                        </FormGroup>
-                    </Col>
-                </Row>
-            </form>
-                
-                <ListGroup>
-                    { this.state.formulationIngredients.map((item, index) => { return this.renderIngredientItem(item, index) }) }   
-                </ListGroup>
+                    <Row>
+                        <Col xs={12} sm={4} md={4} lg={4}>    
+                            
+                        </Col>
+                        <Col xs={12} sm={4} md={4} lg={4}>
+                            <FormGroup
+                            controlId="formSend"
+                            >
+                                <Button bsStyle="primary" onClick={ () => this.handleSendForm() }>Send</Button>
+                            </FormGroup>
+                        </Col>
+
+                        <Col xs={12} sm={4} md={4} lg={4}>    
+                            
+                        </Col>
+                    </Row>
+                </form>
             </div>
         )
     }
